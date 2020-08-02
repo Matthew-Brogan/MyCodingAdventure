b709cef2369e4e952cb1fadbe60b1ae8b5316f58
Harley Rogers <harleydev512@gmail.com>
2020-08-02 09:04:17 -0500

    Added folders for organization, made character interface, character base abstract class, and the different classes classes.

---
+++ MyCodeAdventure/Character/Striker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCodeAdventure.Character
+{
+    class Striker : CharacterBase
+    {
+        public int Power { get; set; }
+    }
+}
---
+++ MyCodeAdventure/Character/Wizard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCodeAdventure.Character
+{
+    class Wizard : CharacterBase
+    {
+        public int Mana { get; set; }
+    }
+}
